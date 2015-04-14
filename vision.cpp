e87c5f0bb5e23ab468e71763824abb8d2c718bd1
JorgeMarAr <jorgemar.ar@gmail.com>
2015-04-13 21:37:38 -0500

    Changes and sizing down of code.
    1. Changed used Mats to only 3: Original (imgOG), Thresholded (imgThresh) and ROI (imgROI)
    2. Renamed thresh_callback to do_ROI, because reasons
    3. Commented out some old imshows that could result in poor performance. Still need to check this
    4. Commented out some printfs that aren't really usefull

--- vision.cpp
+++ vision.cpp
@@ -56,7 +56,7 @@ int read_Video(VideoCapture cap);
 int RGB2HSV();
 int do_Threshold(int, int, int, int, int, int);
 int do_Morph();
-void thresh_callback(int, void*);
+void do_ROI(int, void*);
 
 //Sort function
     //Helper function to sort the areas in descending order
@@ -68,20 +68,20 @@ bool sortByArea(const Moments &lhs, const Moments &rhs)
 //Variables
 int status;
 bool SHOW = true; //'true' = image output, 'false' = NO image output
-//Contour variables
-vector<vector<Point> > contours;
-vector<Vec4i> hierarchy;
 unsigned char MaxCont; //Max contour
 unsigned int Dist; //Distance of the object
 unsigned int RealAr = 3278;
 int thresh = 0;
 int max_thresh = 255;
 RNG rng(12345);
-//Mat
-Mat imgOriginal;
-Mat imgThresholded;
-Mat imgHSV;
-Mat canny_output;
+//Mats
+Mat imgOG;
+Mat imgROI;
+Mat imgThresh;
+//Mat imgOriginal;
+//Mat imgThresholded;
+//Mat imgHSV;
+//Mat canny_output;
 //VideoCapture cap;
 
 int vision_main_thread()//( int argc, char** argv )
@@ -129,8 +129,8 @@ int vision_main_thread()//( int argc, char** argv )
         cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
         //Create trackbar for Canny/Contours
-        //cvCreateTrackbar("Canny thresh:", "Control", &thresh, max_thresh, thresh_callback);
-        createTrackbar("Canny thresh:", "Control", &thresh, max_thresh, thresh_callback);
+        //cvCreateTrackbar("Canny thresh:", "Control", &thresh, max_thresh, do_ROI);
+        createTrackbar("Canny thresh:", "Control", &thresh, max_thresh, do_ROI);
     }
 
     while (true)
@@ -141,7 +141,7 @@ int vision_main_thread()//( int argc, char** argv )
 
         //PREPROCESSING
         //Blur
-        blur(imgHSV, imgHSV, Size(3,3));
+        blur(imgOG, imgOG, Size(3,3));
         //RGB to HSV
         status = RGB2HSV();
         check_Status(status);
@@ -157,12 +157,12 @@ int vision_main_thread()//( int argc, char** argv )
         //MUTE
         if (SHOW)
         {
-            imshow("Thresholded Image", imgThresholded); //show the thresholded image
-            imshow("Original", imgOriginal); //show the original image
+            //imshow("Thresholded Image", imgOG); //show the thresholded image
+            imshow("Original", imgOG); //show the original image
         }
 
         //Identification
-        thresh_callback(0,0);
+        do_ROI(0,0);
 
         //CHECK!
         /*
@@ -225,7 +225,7 @@ int read_Video(VideoCapture cap)
 {
     //Mat imgOriginal;
 
-    bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+    bool bSuccess = cap.read(imgOG); // read a new frame from video
 
     if (!bSuccess) //if not success, break loop
     {
@@ -241,14 +241,14 @@ int read_Video(VideoCapture cap)
 
 int RGB2HSV()
 {
-    cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+    cvtColor(imgOG, imgThresh, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
     //We hope we're good
     return 0;
 }
 
 int do_Threshold(int iLowH, int iHighH, int iLowS, int iHighS, int iLowV, int iHighV)
 {
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
+    inRange(imgThresh, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresh);
     //We hope we're good
     return 0;
 }
@@ -257,67 +257,63 @@ int do_Morph()
 {
     //Check for sizes and Structuring Elements
     //morphological opening (remove small objects from the foreground)
-    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    erode(imgThresh, imgThresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    dilate(imgThresh, imgThresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
     //3x3
     //morphological closing (fill small holes in the foreground)
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    dilate( imgThresh, imgThresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    erode(imgThresh, imgThresh, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
     //eliminar
     //We should be good!
     return 0;
 }
 
-void thresh_callback(int, void* )
+void do_ROI(int, void* )
 {
+    //ROI
     float Xmin=0;
     float Xmax=0;
     float Ymin=0;
     float Ymax=0;
-    Mat canny_output;
-    //GLOBAL NOW!
-    //vector<vector<Point> > contours;
-    //vector<Vec4i> hierarchy;
+    //Canny
+    Mat cannyOut;
+    //Contour
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
 
     //Detect edges using canny
-    //Canny( imgThresholded, canny_output, thresh, thresh*3, 3 );
+    //Canny( imgThresh, cannyOut, thresh, thresh*3, 3 );
         //imgThresholded -> image
         //canny_output -> output
-        //tresh ->
+        //tresh -> Detection threshold
         //tresh*3 -> Recommended value
         //3 -> Kernel size (recommended value)
-    Canny(imgThresholded, canny_output, thresh, thresh*3, 3);
+    Canny(imgThresh, cannyOut, thresh, thresh*3, 3);
 
     //Find contours
-    findContours(canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+    findContours(cannyOut, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
     //Get the moments
     vector<Moments> mu(contours.size() );
     for( size_t i = 0; i < contours.size(); i++ )
     {
         mu[i] = moments( contours[i], false );
-        printf("Got contour %i\n", i);
+        //printf("Got contour %i\n", i);
     }
 
-    //TEMP CHECK!
-    //Print original info
-    for( size_t i = 0; i< contours.size(); i++ )
-    {
-        printf("OG: * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", (int)i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
-    }
     //Sort vector
     sort(mu.begin(), mu.end(), sortByArea);
 
     //Print sorted vector
     for( size_t i = 0; i< contours.size(); i++ )
     {
-        printf("SORT: * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", (int)i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+        printf("SORTED: * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", (int)i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
     }
     //Get the mass centers:
     vector<Point2f> mc( contours.size() );
-    for( size_t i = 0; i < contours.size(); i++ )
+    //for( size_t i = 0; i < contours.size(); i++ )
     //Get only 4
-    //for( size_t i = 0; i < 4; i++ )
+    for( size_t i = 0; i < 4; i++ )
     {
         mc[i] = Point2f( static_cast<float>(mu[i].m10/mu[i].m00) , static_cast<float>(mu[i].m01/mu[i].m00) );
         printf("For contour %i: X=%f, Y=%f\n", i, mc[i].x, mc[i].y);
@@ -326,15 +322,15 @@ void thresh_callback(int, void* )
     //Draw contours
     if (SHOW)
     {
-        Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+        //Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
         for( size_t i = 0; i< contours.size(); i++ )
         {
             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-            drawContours( drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
-            circle( drawing, mc[i], 4, color, -1, 8, 0 );
+            drawContours(imgOG, contours, (int)i, color, 2, 8, hierarchy, 0, Point() );
+            circle(imgOG, mc[i], 4, color, -1, 8, 0 );
         }
-        namedWindow( "Contours", WINDOW_AUTOSIZE );
-        imshow( "Contours", drawing );
+        namedWindow("Contours", WINDOW_AUTOSIZE);
+        imshow("Contours", imgOG);
     }
     //Get coordinates for ROI
         //If for some reason there is no previous ROI, check if there is an error
@@ -345,9 +341,9 @@ void thresh_callback(int, void* )
             //ERROR!
             //Send message
             Xmin=0;
-            Xmax=(float)imgOriginal.cols;
+            Xmax=(float)imgOG.cols;
             Ymin=0;
-            Ymax=(float)imgOriginal.rows;
+            Ymax=(float)imgOG.rows;
         }
         else
         {
@@ -395,9 +391,9 @@ void thresh_callback(int, void* )
         //Nothing found!
         //Send message
         Xmin=0;
-        Xmax=(float)imgOriginal.cols;
+        Xmax=(float)imgOG.cols;
         Ymin=0;
-        Ymax=(float)imgOriginal.rows;
+        Ymax=(float)imgOG.rows;
     }
 
     //Print ROI (x,y)
@@ -409,26 +405,13 @@ void thresh_callback(int, void* )
         //WUT
         //Send message
         Xmin=0;
-        Xmax=(float)imgOriginal.cols;
+        Xmax=(float)imgOG.cols;
         Ymin=0;
-        Ymax=(float)imgOriginal.rows;
+        Ymax=(float)imgOG.rows;
     }
 
     //ROI in original image
     //Rect (Xmin, Ymin, Width, Height)
-    Mat ROI = imgOriginal( Rect (Xmin, Ymin, Xmax-Xmin, Ymax-Ymin) );
-    imshow("ROI", ROI);
-}
-
-int spec_Canny()
-{
-	//Sigma
-	int sigma=0.33;
-
-	float lowCann;
-	float upCann;
-
-	//Get median
-
-
+    imgROI = imgThresh( Rect (Xmin, Ymin, Xmax-Xmin, Ymax-Ymin) );
+    imshow("ROI", imgROI);
 }
